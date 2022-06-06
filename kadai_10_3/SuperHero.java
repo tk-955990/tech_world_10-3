aa1f7c8150650afdb4bf867ce1636405deca4c3b
tk-955990 <105995713+tk-955990@users.noreply.github.com>
2022-06-07 00:24:40 +0900

    kadai 10-3_2022/6/7/0024

--- kadai_10_3/SuperHero.java
+++ kadai_10_3/SuperHero.java
@@ -5,10 +5,10 @@ import kadai.Hero;
 public class SuperHero extends Hero {
 
 	public void attack() {
-		System.out.println(this.getName() + "の攻撃！！");
-		System.out.println("怪物に" + this.getAt() + "ポイントのダメージを与えた！！");
-		System.out.println(this.getName() + "の攻撃！！");
-		System.out.println("怪物に" + this.getAt() + "ポイントのダメージを与えた！！");
+       System.out.println(this.getName() + "の攻撃！！");
+       System.out.println("怪物に" + this.getAt() + "ポイントのダメージを与えた！！");
+       System.out.println(this.getName() + "の攻撃！！");
+       System.out.println("怪物に" + this.getAt() + "ポイントのダメージを与えた！！");
 	}
 
 
